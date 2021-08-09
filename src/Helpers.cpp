57f9e146c809cebbba740ef83c5ea19b8b4b98d3
MoonCordGameJam <51063232+MoonCordGameJam@users.noreply.github.com>
2021-08-09 02:25:45 -0400

    Untabify indentation for PULL Request and revert filename initialization

--- src/Helpers.cpp
+++ src/Helpers.cpp
@@ -12,70 +12,70 @@
 namespace fs = std::filesystem;
 
 std::string randomString(int length) {
-	static auto& characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static auto& characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	thread_local static std::mt19937 rg{std::random_device{}()};
-	thread_local static std::uniform_int_distribution<std::string::size_type> pick(0, sizeof(characters) - 2);
+    thread_local static std::mt19937 rg{std::random_device{}()};
+    thread_local static std::uniform_int_distribution<std::string::size_type> pick(0, sizeof(characters) - 2);
 
-	std::string s;
-	s.reserve(length);
+    std::string s;
+    s.reserve(length);
 
-	for (int i = 0; i < length; i++) {
-		s += characters[pick(rg)];
-	}
+    for (int i = 0; i < length; i++) {
+        s += characters[pick(rg)];
+    }
 
-	return s;
+    return s;
 }
 
 bool fileExists(const std::string& path) {
-	return fs::exists(path);
+    return fs::exists(path);
 }
 
 void readFile(std::string& content, const std::string& path) {
-	std::ifstream stream(path);
-	std::stringstream buffer;
-	buffer << stream.rdbuf();
-	content = std::move(buffer.str());
-	stream.close();
-	content.erase(std::remove(content.begin(), content.end(), '\r'), content.end());
-	content.erase(std::remove(content.begin(), content.end(), '\n'), content.end());
+    std::ifstream stream(path);
+    std::stringstream buffer;
+    buffer << stream.rdbuf();
+    content = std::move(buffer.str());
+    stream.close();
+    content.erase(std::remove(content.begin(), content.end(), '\r'), content.end());
+    content.erase(std::remove(content.begin(), content.end(), '\n'), content.end());
 }
 
 void writeFile(const std::string& name, const std::string& content) {
-	std::ofstream outfile(name);
-	outfile << content;
-	outfile.close();
+    std::ofstream outfile(name);
+    outfile << content;
+    outfile.close();
 }
 
 /*
-	will only remove a file, if a intermediate directory is passed it will fail, could use
-	remove_all and it will remove not only the intermediate directory but also it's sub files
-	return types:
-		remove		= boolean
-		remove_all	= uintmax_t
+    will only remove a file, if a intermediate directory is passed it will fail, could use
+    remove_all and it will remove not only the intermediate directory but also it's sub files
+    return types:
+        remove		= boolean
+        remove_all	= uintmax_t
 */
 bool deleteFile(const std::string& name) {
-	return fs::remove(name);
+    return fs::remove(name);
 }
 
 std::array<char, HASH_LENGTH> convertHash(const std::string& hash) {
-	if (hash.size() != HASH_LENGTH) {
-		std::cerr << "Invalid string when converting hash..." << std::endl;
-		return {};
-	}
+    if (hash.size() != HASH_LENGTH) {
+        std::cerr << "Invalid string when converting hash..." << std::endl;
+        return {};
+    }
 
-	std::array<char, HASH_LENGTH> value {};
-	for (int i = 0; i < HASH_LENGTH; i++) {
-		value[i] = hash[i];
-	}
+    std::array<char, HASH_LENGTH> value {};
+    for (int i = 0; i < HASH_LENGTH; i++) {
+        value[i] = hash[i];
+    }
 
-	return value;
+    return value;
 }
 
 std::string print(std::array<char, HASH_LENGTH> hash) {
-	std::string stringified;
-	stringified.reserve(HASH_LENGTH);
-	std::memcpy(stringified.data(), hash.data(), HASH_LENGTH);
+    std::string stringified;
+    stringified.reserve(HASH_LENGTH);
+    std::memcpy(stringified.data(), hash.data(), HASH_LENGTH);
 
-	return stringified;
+    return stringified;
 }
